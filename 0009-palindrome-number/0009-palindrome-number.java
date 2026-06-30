5a021217f714105dbbfeb34ca53e4e27780a76e6
Karan2201 <karanrathore7000@gmail.com>
2026-06-30 10:01:04 +0530

    Time: 5 ms (79.59%), Space: 46.2 MB (17.2%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        // Also, if last digit is 0, the first digit must be 0 (only possible for 0)
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reversed = 0;
+        int original = x;
+        
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        
+        return original == reversed;
+    }
+}
